27930343fd17cec0bfe1d8e1bb86ccb6ee49e4d6
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-08-30 22:24:14 +0300

    Added images to sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,9 +1,17 @@
 import './index.scss'
+import { Link } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 
 const Sidebar = () => {
     return (
         <>
-            Hello from Sidebar
+            <div className="nav-bar">
+                <Link className="logo" to="/">
+                    <img src={LogoS} alt="logo" />
+                    <img src={LogoSubtitle} alt="slobodan" />
+                </Link>
+            </div>
         </>
     )
 }
