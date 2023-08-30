1bc2754a30c26675c48f1cfba02f9538009b2c2d
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-08-30 23:39:12 +0300

    Styled links to my socials

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
     return (
@@ -38,6 +39,44 @@ const Sidebar = () => {
                         <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
                     </NavLink>
                 </nav>
+                <ul>
+                    <li>
+                        <a
+                            target='_blank'
+                            rel='noreferrer'
+                            href="https://www.linkedin.com/in/joseph-akumu-77a5831a9/">
+                            <FontAwesomeIcon icon={faLinkedin} color='#4d4d4e'
+                        />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            target='_blank'
+                            rel='noreferrer'
+                            href="https://github.com/AkumuJey">
+                            <FontAwesomeIcon icon={faGithub} color='#4d4d4e'
+                        />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            target='_blank'
+                            rel='noreferrer'
+                            href="https://twitter.com/akumujey">
+                            <FontAwesomeIcon icon={faTwitter} color='#4d4d4e'
+                        />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            target='_blank'
+                            rel='noreferrer'
+                            href="https://www.instagram.com/akumujey/">
+                            <FontAwesomeIcon icon={faInstagram} color='#4d4d4e'
+                        />
+                        </a>
+                    </li>
+                </ul>
             </div>
         </>
     )
